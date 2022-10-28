96db213bf599eaab0a41744596a93b051efc5c2d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 05:14:23 +0000

    Adding tarfile member sanitization to extractall()

--- main.py
+++ main.py
@@ -37,7 +37,26 @@ def publish_release(ruffle_version, version_to_publish):
 
     # untar ruffle dist
     with tarfile.open(os.path.join(dist_temp_folder.name, 'ruffle.tar.gz'), 'r:gz') as tar:
-        tar.extractall(temp_folder_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, temp_folder_path)
 
     # write package.json to temp dir
     with open(PACKAGE_JSON_TEMPLATE) as f:
